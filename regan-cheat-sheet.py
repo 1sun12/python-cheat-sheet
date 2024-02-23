066d482196111fcddf8a25975f03d65820287180
1sun12 <regan_sun12@outlook.com>
2024-02-23 09:16:07 -0600

    script with just comments

---
+++ regan-cheat-sheet.py
@@ -0,0 +1,7 @@
+# @Title:   ICPC-Cheat-Sheet
+# @Author:  Regan O'Donnell
+# @DoFB:    2/23/2024
+
+# @Desc: A "python 101" intended manuscript containing all basic to complex things (that I could think of) you can do in python
+# This sheet also acts as a refresher if I ever take long breaks from python
+
